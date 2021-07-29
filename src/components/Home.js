133a67e65047409169fed13d2302c3bcdc65c348
Pauline122112 <pauline.design007@gmail.com>
2021-07-29 12:43:10 -0500

    adding style components

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,32 +7,95 @@ import ClassesList from "./ClassesList";
 import styled, { createGlobalStyle, css } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-`
+html {
+	height: 100%;
+	
+
+}
+
+body {
+	font-family: Arial, Helvetica, sans-serif;
+	background: linear-gradient(to bottom, #24C6DC, #514A9D);
+	height: 100%;
+	margin: 0;
+	color: #555;
+
+}
+	
+
+h2 {
+	text-align: center;
+}
+
+
+`;
 
 const sharedStyles = css`
-`
+background-color: #eee;
+height: 40px;
+border-radius: 5px;
+border: 1px sold #ddd;
+margin: 10px 0 20px 0;
+padding: 20px;
+box-sizing: border-box;
+`;
 
 const StyledFormWrapper = styled.div`
-`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+padding: 0 20px;
+
+`;
 
 const StyledForm = styled.form`
-`
+width: 100%;
+max-width: 700px;
+padding: 40px;
+background-color: #ffffff;
+border-radius: 10px;
+box-sizing: border-box;
+box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
+`;
 
-const StyledInput = styled.input`
-`
+const StyledInput = styled.div`
+display: block;
+width: 100%;
+${sharedStyles}
+`;
 
-const StyledTextArea = styled.textarea`
-`
+const StyledLocation = styled.label`
+`;
 
 const StyledButton = styled.button`
-`
+display: block;
+background-color:  #f7797d;
+color: #fff;
+font-size: .9rem;
+border: 0;
+border-radius: 5px;
+height: 40px;
+padding: 0 20px;
+cursor: pointer;
+box-sizing: border-box;
 
-const StyledFieldset = styled.fieldset`
-`
+`;
 
-const StyledError = styled.div`
-`
+const StyledStartTime = styled.label`
+`;
 
+const StyledStartDate = styled.label`
+`;
+
+const StyledIntensity = styled.label`
+`;
+
+const StyledMaximum = styled.label`
+`;
+
+const StyledList = styled.div`
+`; 
 
 const defaultSearch = {
     name: "",
@@ -65,119 +128,130 @@ export default function Home() {
     }, [])
 
     return (
+			<div>
+				<>
+					<GlobalStyle />
+					<h2>Class Timetable</h2>
+					<StyledFormWrapper>
+						<StyledForm onSubmit={(e) => e.preventDefault()}>
+							<StyledInput>
+								<label htmlFor="name">
+									Name:
+									<input
+										data-cy="class-name"
+										type="text"
+										name="name"
+										id="name"
+										value={formValues.name}
+										onChange={change}
+									/>
+								</label>
+							</StyledInput>
 
+							<StyledInput>
+								<label htmlFor="type">
+									Type:
+									<input
+										data-cy="class-type"
+										type="text"
+										name="type"
+										id="type"
+										value={formValues.type}
+										onChange={change}
+									/>
+								</label>
+							</StyledInput>
 
-        <div>
-        <>
-        <GlobalStyle />
-            <StyledFormWrapper>
-            <StyledForm>
-        
-        
-            <h2>Class Timetable</h2>
-            <form onSubmit={e => e.preventDefault()}>
-                <label htmlFor="name">
-                    Name:
-                    <StyledInput 
-                        data-cy="class-name"
-                        type="text"
-                        name="name"
-                        id="name"
-                        value={formValues.name}
-                        onChange={change}
-                    />
-                </label>
-                <label htmlFor="type">
-                    Type:
-                    <StyledInput 
-                        data-cy="class-type"
-                        type="text"
-                        name="type"
-                        id="type"
-                        value={formValues.type}
-                        onChange={change}
-                    />
-                </label>
-                <StyledFieldset>
-                <label htmlFor="startTime">
-                    Start Time:
-                    <input 
-                        data-cy="class-time"
-                        type="time"
-                        name="startTime"
-                        id="startTime"
-                        value={formValues.startTime}
-                        onChange={change}
-                    />
-                </label>
-                </StyledFieldset>
+							<StyledInput>
+								<StyledStartTime>
+									<label htmlFor="startTime">
+										Start Time:
+										<input
+											data-cy="class-time"
+											type="time"
+											name="startTime"
+											id="startTime"
+											value={formValues.startTime}
+											onChange={change}
+										/>
+									</label>
+								</StyledStartTime>
+							</StyledInput>
 
-                <label for="startDate">
-                    Start Date:
-                    <input 
-                        data-cy="class-date"
-                        type="date"
-                        name="startDate"
-                        id="startDate"
-                        value={formValues.startDate}
-                        onChange={change}
-                    />
-                </label>
-                <label for="location">
-                    Location:
-                    <input 
-                        data-cy="class-location"
-                        type="text"
-                        name="location"
-                        id="location"
-                        value={formValues.location}
-                        onChange={change}
-                    />
-                </label>
-                <label for="duration">
-                    Duration (minutes):
-                    <input 
-                        step={5}
-                        data-cy="class-duration"
-                        type="number"
-                        name="duration"
-                        id="duration"
-                        value={formValues.duration}
-                        onChange={change}
-                    />
-                </label>
-                <label for="intensity">
-                    Intensity (1 to 10):
-                    <input 
-                        data-cy="class-intensity"
-                        type="number"
-                        name="intensity"
-                        id="intensity"
-                        value={formValues.intensity}
-                        onChange={change}
-                    />
-                </label>
-                <label for="max">
-                    Max participants:
-                    <input 
-                        data-cy="class-max"
-                        type="text"
-                        name="max"
-                        id="max"
-                        value={formValues.max}
-                        onChange={change}
-                    />
-                </label>
-                <StyledButton 
-                    data-cy="reset"
-                    onClick={reset} 
-                    type="reset"
-                >Reset</StyledButton>
-            </form>
-            <ClassesList classes={filteredData} />
-            </StyledForm>
-            </StyledFormWrapper>
-            </>
-        </div>
-    )
+							<StyledInput>
+								<StyledStartDate>
+									<label htmlFor="startDate">
+										Start Date:
+										<input
+											data-cy="class-date"
+											type="date"
+											name="startDate"
+											id="startDate"
+											value={formValues.startDate}
+											onChange={change}
+										/>
+									</label>
+								</StyledStartDate>
+							</StyledInput>
+
+							<StyledInput>
+								<StyledLocation>
+									<label for="location">
+										Location:
+										<input
+											data-cy="class-location"
+											type="text"
+											name="location"
+											id="location"
+											value={formValues.location}
+											onChange={change}
+										/>
+									</label>
+								</StyledLocation>
+							</StyledInput>
+
+							<StyledInput>
+								<StyledIntensity>
+									<label for="intensity">
+										Intensity (1 to 10):
+										<input
+											data-cy="class-intensity"
+											type="number"
+											name="intensity"
+											id="intensity"
+											value={formValues.intensity}
+											onChange={change}
+										/>
+									</label>
+								</StyledIntensity>
+							</StyledInput>
+
+							<StyledInput>
+								<StyledMaximum>
+									<label for="max">
+										Max participants:
+										<input
+											data-cy="class-max"
+											type="text"
+											name="max"
+											id="max"
+											value={formValues.max}
+											onChange={change}
+										/>
+									</label>
+								</StyledMaximum>
+							</StyledInput>
+
+							<StyledButton data-cy="reset" onClick={reset} type="reset">
+								Reset
+							</StyledButton>
+						</StyledForm>
+
+						<StyledList>
+							<ClassesList classes={filteredData} />
+						</StyledList>
+					</StyledFormWrapper>
+				</>
+			</div>
+		);
 }
