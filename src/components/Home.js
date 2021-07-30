fb99cc7ea218c7f829e8152fb36b71b78b907fbf
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-07-30 12:15:32 -0400

    m

--- src/components/Home.js
+++ src/components/Home.js
@@ -32,38 +32,37 @@ h2 {
 `;
 
 const sharedStyles = css`
-background-color: #eee;
-
-border-radius: 5px;
-border: 1px sold #ddd;
-margin: 10px 0 20px 0;
-padding: 20px;
-box-sizing: border-box;
+	background-color: #eee;
+	border-radius: 5px;
+	border: 1px sold #ddd;
+	margin: 10px 0 20px 0;
+	padding: 20px;
+	box-sizing: border-box;
 `;
 
 const StyledFormWrapper = styled.div`
-display: flex;
-justify-content: center;
-align-items: center;
-height: 100vh;
-padding: 0 20px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	padding: 0 20px;
 
 `;
 
 const StyledForm = styled.form`
-width: 100%;
-max-width: 700px;
-padding: 40px;
+	width: 100%;
+	max-width: 700px;
+	padding: 40px;
 
-border-radius: 10px;
-box-sizing: border-box;
-box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
+	border-radius: 10px;
+	box-sizing: border-box;
+	box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
 `;
 
 const StyledInput = styled.div`
-display: block;
-width: 100%;
-${sharedStyles};
+	display: block;
+	width: 100%;
+	${sharedStyles};
 
 
 `;
@@ -72,16 +71,16 @@ const StyledLocation = styled.label`
 `;
 
 const StyledButton = styled.button`
-display: block;
-background-color:  #f7797d;
-color: #fff;
-font-size: .9rem;
-border: 0;
-border-radius: 5px;
-height: 40px;
-padding: 0 20px;
-cursor: pointer;
-box-sizing: border-box;
+	display: block;
+	background-color:  #f7797d;
+	color: #fff;
+	font-size: .9rem;
+	border: 0;
+	border-radius: 5px;
+	height: 40px;
+	padding: 0 20px;
+	cursor: pointer;
+	box-sizing: border-box;
 
 `;
 
